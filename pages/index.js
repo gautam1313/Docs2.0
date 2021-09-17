eae918edecf06f05dab4cb43b819a753362c2bd1
Sebby80 <sebkm80@gmail.com>
2021-09-17 16:05:15 +0530

    Adding section for blank document design

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
 
 export default function Home() {
   return (
@@ -9,6 +11,34 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <section className="bg-[#F8F9FA] pb-10 px-10">
+        <div className="max-w-3xl mx-auto">
+          <div className="flex items-center justify-between py-6">
+            <h2 className="text-gray-700 text-lg">Start a new document</h2>
+            <Button
+              color="gray"
+              buttonType="outline"
+              iconOnly={true}
+              ripple="dark"
+              className="border-0"
+            >
+              <Icon name="more_vert" size="3xl" />
+            </Button>
+          </div>
+          <div>
+            <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700">
+              <img
+                src="https://ssl.gstatic.com/docs/templates/thumbnails/docs-blank-googlecolors.png"
+                alt=""
+                loading="lazy"
+              />
+            </div>
+            <p className="mt-2 ml-2 font-semibold text-sm text-gray-700">
+              Blank
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
